c5ac68f994ef46a8614481f1872450f30d61a5aa
arturlevickij <arturlevickij@gmail.com>
2022-11-30 21:48:56 +0300

    adding 2 files for laboratorna 5

---
+++ Header.h
@@ -0,0 +1,145 @@
+#include <iostream>
+using namespace std;
+
+enum Type {
+	ISOTYPE,
+	RADIOACTIVE,
+	ION,
+	ANTIMATTER,
+	STABLE
+};
+
+class Atom {
+
+	string name;
+	int atomic_mass_unit;
+	int neutrons_number;
+	int protons_number;
+	int electrons_number;
+	Type atom_type;
+
+public:
+
+
+	Atom() {
+
+	}
+
+	Atom(string name, int atomic_mass_unit, int neutrons_number, int protons_number, int electrons_number) {
+		this->name = name;
+		this->atomic_mass_unit = atomic_mass_unit;
+		this->neutrons_number = neutrons_number;
+		this->protons_number = protons_number;
+		this->electrons_number = electrons_number;
+	}
+
+
+	Type get_atomtype() {
+		return atom_type;
+	}
+	string get_name() {
+		return name;
+	}
+	int get_atommass() {
+		return atomic_mass_unit;
+	}
+	int get_neutron() {
+		return neutrons_number;
+	}
+	int get_proton() {
+		return protons_number;
+	}
+	int get_electron() {
+		return electrons_number;
+	}
+	string set_name() {
+		this->name = name;
+		return name;
+	}
+	void set_atom_type(Type atom_type) {
+		this->atom_type = atom_type;
+	}
+	int set_atommass() {
+		this->atomic_mass_unit = atomic_mass_unit;
+		return atomic_mass_unit;
+	}
+	int set_neutron() {
+		this->neutrons_number = neutrons_number;
+		return neutrons_number;
+	}
+	int set_proton() {
+		this->protons_number = protons_number;
+		return protons_number;
+	}
+	int set_electron() {
+		this->electrons_number = electrons_number;
+		return electrons_number;
+	}
+
+
+	~Atom() {}
+
+	bool isNeutral() {
+		if (get_electron() != get_neutron()) {
+			cout << "false = ";
+			return false;
+		}
+		else {
+			cout << "true = ";
+			return true;
+		}
+	}
+
+};
+
+
+class Molecule {
+
+public:
+	Atom array[5];
+	float res = 0;
+
+	void swap(Atom* xp, Atom* yp)
+	{
+		Atom temp = *xp;
+		*xp = *yp;
+		*yp = temp;
+	}
+
+	void SortAtoms(int size) {
+		{
+			for (int i = 0; i < size - 1; i++) {
+				for (int j = 0; j < size - i - 1; j++) {
+					if (array[j].get_atommass() > array[j + 1].get_atommass()) {
+						swap(&array[j], &array[j + 1]);
+					}
+				}
+			}
+			for (int k = 0; k < size; k++) {
+				cout << "Name = " << array[k].get_name() << endl;
+				cout << "Atom mass = " << array[k].get_atommass() << endl;
+				cout << "Neutron num = " << array[k].get_neutron() << endl;
+				cout << "Proton num = " << array[k].get_proton() << endl;
+				cout << "Electron num = " << array[k].get_electron() << endl;
+				cout << "-------------------" << endl;
+
+			}
+		}
+	}
+
+	void findAverageMass(int size) {
+		for (int i = 0; i < size; i++) {
+			res += array[i].get_atommass();
+		}
+		res = res / size;
+		cout << res << endl;
+	}
+
+	void Print(int b) {
+		cout << "Name = " << array[b].get_name() << endl;
+		cout << "Atom mass = " << array[b].get_atommass() << endl;
+		cout << "Neutron num = " << array[b].get_neutron() << endl;
+		cout << "Proton num = " << array[b].get_proton() << endl;
+		cout << "Electron num = " << array[b].get_electron() << endl;
+	}
+};
---
+++ Laba5.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include "Header.h"
+using namespace std;
+
+int main()
+{
+	Atom azot("Azot", 23, 12, 11, 12);
+	Atom oxygen("Oxygen", 16, 11, 16, 5);
+	Atom hydrogen("Hydrogen", 50, 17, 20, 18);
+	Atom yod("Yod", 61, 34, 14, 34);
+	Atom ftor("Ftor", 43, 27, 18, 7);
+
+	Molecule mol;
+
+	mol.array[0] = azot;
+	mol.array[1] = oxygen;
+	mol.array[2] = hydrogen;
+	mol.array[3] = yod;
+	mol.array[4] = ftor;
+
+	azot.set_atom_type(Type::ION);
+	oxygen.set_atom_type(Type::RADIOACTIVE);
+	hydrogen.set_atom_type(Type::ISOTYPE);
+	yod.set_atom_type(Type::STABLE);
+	ftor.set_atom_type(Type::ANTIMATTER);
+
+
+	if (azot.get_atomtype() == Type::ION) {
+		cout << "Azot is ion" << endl;
+	}
+	if (oxygen.get_atomtype() == Type::RADIOACTIVE) {
+		cout << "Oxygen is RADIOACTIVE" << endl;
+	}
+	if (hydrogen.get_atomtype() == Type::ISOTYPE) {
+		cout << "Hydrogen is ISOTYPE" << endl;
+	}
+	if (yod.get_atomtype() == Type::STABLE) {
+		cout << "Yod is Stable" << endl;
+	}
+	if (ftor.get_atomtype() == Type::ANTIMATTER) {
+		cout << "Ftor is ANTIMATTER" << endl;
+	}
+
+	cout << "\n";
+
+
+	const int s = sizeof mol / sizeof mol.array[0];
+
+	for (int i = 0; i < s; i++) {
+		mol.Print(i);
+		cout << mol.array[i].isNeutral() << endl;
+		cout << "-------------------" << endl;
+	}
+	cout << "AVG Mass = ";
+	mol.findAverageMass(s);
+
+	cout << "---------Sort by Atom mass--------" << endl;
+	mol.SortAtoms(s);
+	cout << endl;
+
+}
+
+
+
