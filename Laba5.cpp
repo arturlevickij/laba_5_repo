e569639639723c27f8d91480503eb2b2def016f2
arturlevickij <74044788+arturlevickij@users.noreply.github.com>
2022-12-21 14:37:33 +0300

    fixed

--- Laba5.cpp
+++ Laba5.cpp
@@ -4,40 +4,40 @@ using namespace std;
 
 int main()
 {
-	Atom azot("Azot", 23.5, 12.5, 11.1, 12.7);
-	Atom oxygen("Oxygen", 16.1, 11.7, 16.3, 5.1);
-	Atom hydrogen("Hydrogen", 50.3, 17.5, 20.5, 18.7);
-	Atom yod("Yod", 61.7, 34.7, 14.1, 34.7);
-	Atom ftor("Ftor", 43.9, 27.5, 18.5, 7.1);
+	Atom Azot("Azot", 23.5, 12.5, 11.1, 12.7);
+	Atom Oxygen("Oxygen", 16.1, 11.7, 16.3, 5.1);
+	Atom Hydrogen("Hydrogen", 50.3, 17.5, 20.5, 18.7);
+	Atom Yod("Yod", 61.7, 34.7, 14.1, 34.7);
+	Atom Ftor("Ftor", 43.9, 27.5, 18.5, 7.1);
 
 	Molecule moleculate;
 
-	moleculate.array[0] = azot;
-	moleculate.array[1] = oxygen;
-	moleculate.array[2] = hydrogen;
-	moleculate.array[3] = yod;
-	moleculate.array[4] = ftor;
+	moleculate.array[0] = Azot;
+	moleculate.array[1] = Oxygen;
+	moleculate.array[2] = Hydrogen;
+	moleculate.array[3] = Yod;
+	moleculate.array[4] = Ftor;
 
-	azot.set_atom_type(Type::ION);
-	oxygen.set_atom_type(Type::RADIOACTIVE);
-	hydrogen.set_atom_type(Type::ISOTYPE);
-	yod.set_atom_type(Type::STABLE);
-	ftor.set_atom_type(Type::ANTIMATTER);
+	Azot.set_atom_type(Type::ION);
+	Oxygen.set_atom_type(Type::RADIOACTIVE);
+	Hydrogen.set_atom_type(Type::ISOTYPE);
+	Yod.set_atom_type(Type::STABLE);
+	Ftor.set_atom_type(Type::ANTIMATTER);
 
 
-	if (azot.get_atomtype() == Type::ION) {
+	if (Azot.get_atomtype() == Type::ION) {
 		cout << "Azot is ion" << endl;
 	}
-	if (oxygen.get_atomtype() == Type::RADIOACTIVE) {
+	if (Oxygen.get_atomtype() == Type::RADIOACTIVE) {
 		cout << "Oxygen is RADIOACTIVE" << endl;
 	}
-	if (hydrogen.get_atomtype() == Type::ISOTYPE) {
+	if (Hydrogen.get_atomtype() == Type::ISOTYPE) {
 		cout << "Hydrogen is ISOTYPE" << endl;
 	}
-	if (yod.get_atomtype() == Type::STABLE) {
+	if (Yod.get_atomtype() == Type::STABLE) {
 		cout << "Yod is Stable" << endl;
 	}
-	if (ftor.get_atomtype() == Type::ANTIMATTER) {
+	if (Ftor.get_atomtype() == Type::ANTIMATTER) {
 		cout << "Ftor is ANTIMATTER" << endl;
 	}
 
